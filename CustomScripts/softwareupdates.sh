8984cbb1c2dd4b65bd0f5fb37314d0803c07fd01
Vince Mascoli <v@vincemascoli.com>
2017-06-14 07:51:26 -0700

    Updated softwareupdate.sh
    Updated softwareupdate.sh to use the bash lib file for hello-it.

--- CustomScripts/softwareupdates.sh
+++ CustomScripts/softwareupdates.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+# Pending Updates Script for Managed Software Center
+### The following line load the Hello IT bash script lib
+. "$HELLO_IT_SCRIPT_FOLDER/com.github.ygini.hello-it.scriptlib.sh"
 
-#Hello-IT script to display the number of software updates available
+# Functions
+# updateTitle "String" - Updates Menu Item Title (Text of item)
+# updateTooltip "String" - Updates Menu Item Hover Text
+# updateState ${STATE[0]} - Updates the state icon
+# STATE VALUES
+# supported states are managed by the STATE array
+# ${STATE[0]} --> OK (Green light)
+# ${STATE[1]} --> Warning (Orange light)
+# ${STATE[2]} --> Error (Red light)
+# ${STATE[3]} --> Unavailable (Empty circle)
+# ${STATE[4]} --> No state to display (Nothing at all)
 
 numberOfUpdates=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate LastRecommendedUpdatesAvailable)
 
-if [[ $numberOfUpdates > 0 ]]
-then
-  echo "hitp-enabled: YES"
-  echo "hitp-hidden: NO"
-  echo "hitp-title: Software Updates: $numberOfUpdates"
-else
-  echo "hitp-enabled: YES"
-  echo "hitp-hidden: NO"
-  echo "hitp-title: No Updates Available"
-fi
+function onClickAction {
+  open macappstore://showUpdatesPage
+}
+
+function fromCronAction {
+  setTitleAction "$@"
+}
+
+function setTitleAction {
+  if [[ $numberOfUpdates > 0 ]]
+  then
+    updateTitle "Software Updates: $numberOfUpdates"
+  else
+    updateTitle "No Updates Available"
+  fi
+}
+
+### The only things to do outside of a bash function is to call the main function defined by the Hello IT bash lib.
+main "$@"
 
 exit 0
